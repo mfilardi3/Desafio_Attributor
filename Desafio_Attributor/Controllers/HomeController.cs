9f8984960cdd51701c7d9b98ad8d137a56c8900c
mfilardi3 <mfilardi3@gmail.com>
2019-05-17 13:30:19 -0300

    Estrutura codificada

--- Desafio_Attributor/Controllers/HomeController.cs
+++ Desafio_Attributor/Controllers/HomeController.cs
@@ -6,25 +6,24 @@ using System.Web.Mvc;
 
 namespace Desafio_Attributor.Controllers
 {
+    //controlador da Home
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            return View();
+            return View(); //retorna exibição da view Index
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Startup de tecnologia especializada em recuperação de impostos para empresas do Simples Nacional.";
 
-            return View();
+            return View(); //retorna exibição da view About
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return View(); //retorna exibição da view Contact
         }
     }
 }
